31ace8d2baf2275623550f2ee2ff94ae6a5db005
farroshayray <farros.hr@gmail.com>
2024-07-06 21:02:15 +0700

    creating layouts

---
+++ index.js
@@ -0,0 +1,25 @@
+// JavaScript function to show the container when hovering over "Apps"
+function showContainer1() {
+    document.getElementById("under-nav1").style.display = "block";
+}
+function showContainer1() {
+    document.getElementById("under-nav1").style.display = "block";
+}
+function showContainer2() {
+    document.getElementById("under-nav2").style.display = "block";
+}
+function showContainer2() {
+    document.getElementById("under-nav2").style.display = "block";
+}
+function showContainer3() {
+    document.getElementById("under-nav3").style.display = "block";
+}
+function showContainer3() {
+    document.getElementById("under-nav3").style.display = "block";
+}
+function showContainer4() {
+    document.getElementById("under-nav4").style.display = "block";
+}
+function showContainer4() {
+    document.getElementById("under-nav4").style.display = "block";
+}
